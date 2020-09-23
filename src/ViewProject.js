cd19333762a6fbd654894006a4b762acfa660d6d
Peter Briggeman <pbriggeman@gmail.com>
2020-09-22 21:29:40 -0400

    ProjectView functions working corretly

--- src/ViewProject.js
+++ src/ViewProject.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withFirebase } from './FirebaseContextInit';
+import { useParams } from "react-router-dom";
 import './ViewProject.css';
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -44,100 +45,33 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function Details(props) {
-  // const [isFetching, setFetching] = useState( false );
-  console.log("DETAILS!" + props.pid);  
-
-  // var documentReference = props.firebase.projects().doc('1C7k9hcsKpxWxOe0MbW2');
-  // documentReference.get().then(function(documentSnapshot) {
-  //   if (documentSnapshot.exists) {
-  //     console.log('document found');
-  //     var data = documentSnapshot.data();
-  //     console.log('DATA :' + data.title);
-  //   } else {
-  //     console.log('document not found');
-  //   }
-  // });
-
-
-  
-  // const projectRef = props.firebase.projects().doc('1C7k9hcsKpxWxOe0MbW2');
-  // const doc = await projectRef.get().then();
-
-
-  // props.firebase.projects()
-  //   .doc('1C7k9hcsKpxWxOe0MbW2')
-  //   .get()
-  //   .then(snapshot => {
-      
-
-  //     // if (!snapshot.empty) {
-  //     //   console.log("snapshot not empty");
-  //     //   // const data = snapshot.docs.map(doc => doc.data());
-  //     //   console.log(snapshot);
-  //     // }
-  //     // else {
-  //     //   console.log("snapshot empty");
-  //     // }
-      
-  //     // var contentKeys = Object.keys(querySnapshot.fE);
-  //     // console.log("DATA: " + querySnapshot.data.description);
-  //     // const data = querySnapshot.doc;
-  //     // querySnapshot.forEach((queryDocumentSnapshot) => {
-  //     //   console.log(queryDocumentSnapshot.id);
-  //     // });
-  //     //console.log("querySnapshot: " + data);
-  //     // this.setState({
-  //     //   projects: data,
-  //     //   loading: false
-  //     // });
-
-  //     if (!snapshot.empty) {
-  //       console.log("snapshot not empty");
-  //       snapshot.docs.forEach ((doc) => {
-  //         const data = doc.data();
-  //         console.log("TITLE :" + data[0].title);
-  //       });
-  //     }
-  //     else {
-  //       console.log("snapshot empty");
-  //     }
-
-
-  //     // snapshot.forEach(doc => {
-  //     //   const data = doc.data();
-  //     //   console.log(doc.id, data);
-  //     // });
-  //   });
-
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      Test
-    </Typography>
-  );
-}
-
 function ViewProject(props) {
   const classes = useStyles();
+  const { id } = useParams();
   const [loading, setLoading] = React.useState(true);
+  const [data, setData] = React.useState([]);
+  const [hasSucceeded, setSuccess] = React.useState(false);
 
-  var documentReference = props.firebase.projects().doc('1C7k9hcsKpxWxOe0MbW2');
-  documentReference.get().then(function(documentSnapshot) {
-    if (documentSnapshot.exists) {
-      console.log('document found');
-      var data = documentSnapshot.data();
-      console.log('DATA :' + data.title);
-    } else {
-      console.log('document not found');
-    }
-    setLoading(false);
-  });
- 
+  if (!hasSucceeded) {
+    var documentReference = props.firebase.projects().doc(id);
+    documentReference.get().then(function(documentSnapshot) {
+      if (documentSnapshot.exists) {
+        console.log('document found');
+        setData(documentSnapshot.data());
+        setSuccess(true);
+        console.log('DATA :' + data.title);
+      } else {
+        console.log('document not found');
+      }
+      setLoading(false);
+    });
+  }
+  
   return (  
     <div className={classes.heroContent}>
       { loading && <CircularProgress /> }
       <Typography variant="body2" color="textSecondary" align="center">
-        Test
+        Title: {data.title}
       </Typography>   
     </div>
   );
